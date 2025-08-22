45e4d1987e7a0ee8d4f7eeaf3d2d87d0e9778b19
2NMaster <2NMaster@DESKTOP-KL01N7V>
2025-08-22 16:21:39 +0300

    .

--- src/app/components/layout/vertical/header/Profile/Profile.tsx
+++ src/app/components/layout/vertical/header/Profile/Profile.tsx
@@ -100,7 +100,7 @@ const Profile = () => {
               Admin
             </Typography>
             <Typography variant="subtitle2" color="textSecondary">
-              Admin
+              Fas Admin
             </Typography>
             <Typography
               variant="subtitle2"
