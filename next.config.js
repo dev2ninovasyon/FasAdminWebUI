15424b59e9cb71e07f0dd840d63f9263309defb5
Your Name <melisakpl@hotmail.com>
2026-01-17 16:13:34 +0300

    feat: Replace modularizeImports with transpilePackages and optimizePackageImports for MUI and Tabler Icons.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  modularizeImports: {
-    "@mui/icons-material": {
-      transform: "@mui/icons-material/{{member}}",
-    },
-    // TODO: Consider enabling modularizeImports for material when https://github.com/mui/material-ui/issues/36218 is resolved
-    // '@mui/material': {
-    //   transform: '@mui/material/{{member}}',
-    // },
+  transpilePackages: [
+    "@mui/material",
+    "@mui/system",
+    "@mui/icons-material",
+    "@tabler/icons-react",
+  ],
+  experimental: {
+    optimizePackageImports: [
+      "@mui/material",
+      "@mui/icons-material",
+      "@tabler/icons-react",
+    ],
   },
   reactStrictMode: false,
   distDir: "build",
